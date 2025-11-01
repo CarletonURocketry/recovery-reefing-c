2fe8dbf0756cc5565937e4298f814cc64d0dd64e
Azao-dev <azanhg02@gmail.com>
2025-11-01 13:38:30 -0400

    Update Make-CSV.c

--- C pico codes/Make-CSV.c
+++ C pico codes/Make-CSV.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#include <time.h>
 
 int main(){
     FILE *fpt;
     char filename[] = "NewFile.csv";
+    time_t curr_time;
 
     fpt = fopen(filename, "w+");
+    time(&curr_time); // we may have to do this every time we get a new time to clock
+    
 
-    fprintf(fpt, "This is a test\n");
+    fprintf(fpt, "Current Time: %s", ctime(&curr_time));
 
     fclose(fpt);
     return 0;
