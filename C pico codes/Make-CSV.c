fdcb92cc40d0d6915cba0a982ae58865912f4265
Azao-dev <azanhg02@gmail.com>
2025-11-04 17:41:02 -0500

    Update Make-CSV.c

--- C pico codes/Make-CSV.c
+++ C pico codes/Make-CSV.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <datetimeapi.h>
 #include <time.h>
 
+void record_data(FILE *fpt, char string[]){
+    time_t curr_time; // create time variable
+    time(&curr_time); // get current time
+    char *time = ctime(&curr_time); // turn it into a string
+
+    if(time[strlen(time) - 1] = "\n"){ // if time has a \n at the end it will make the csv look ugly, this fixes that
+        time[strlen(time) - 1] = "\0";
+    }
+
+    fprintf(fpt, "Current Time: %s --------------- %s", time, string); // add line of text to CSV
+    return;
+}
+
 int main(){
-    FILE *fpt;
-    char filename[] = "NewFile.csv";
+    FILE *fpt; // creates CSV file variable
+    char filename[] = "NewFile.csv"; // name of CSV file
     time_t curr_time;
 
-    fpt = fopen(filename, "w+");
-    time(&curr_time); // we may have to do this every time we get a new time to clock
+    fpt = fopen(filename, "w+"); // Attaches CSV file to CSV variable
     
-
-    fprintf(fpt, "Current Time: %s", ctime(&curr_time));
+    record_data(fpt, "Test has worked"); // Function to call whenever data needs to be added to csv
 
     fclose(fpt);
     return 0;
