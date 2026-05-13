b6181529d6cfeee291725d152835409acebc7db8
Jack Timmons <jacktimmons10@gmail.com>
2026-05-13 14:46:45 -0400

    Update EmatchIgnition.c

--- .vscode/GPIO STUFF/EmatchIgnition.c
+++ .vscode/GPIO STUFF/EmatchIgnition.c
@@ -9,33 +9,19 @@
 
 #define ALTIMETERPIN 28
 #define EMATCHPIN 10
+#define LED1 11
+#define LED2 12
+#define LED3 13
 
-/**
- * Main function
- */
-int main(){
-    init();
-    
-    while(true){
-        if (gpio_get(ALTIMETERPIN)){
-            gpio_put(EMATCHPIN, 1);
-        }
-    }
-
-    return 0;
+void init_leds(){
+    gpio_init(LED1);
+    gpio_set_dir(LED1, GPIO_OUT);
+    gpio_init(LED2);
+    gpio_set_dir(LED2, GPIO_OUT);
+    gpio_init(LED3);
+    gpio_set_dir(LED3, GPIO_OUT);
 }
 
-
-/**
- * Fuction to set up all pins and their directions 
- * (this would be ~50 lines without the libraries)
- */
-void init(){
-    stdio_init_all();
-
-    init_ematch_pin();
-    init_altimeter_pin();
-}
 
 void init_ematch_pin(){
     gpio_init(EMATCHPIN);
@@ -49,3 +35,61 @@ void init_altimeter_pin(){
     // Altimeter is pull up; 1 when IN == 0, 0 when IN == 1
     gpio_pull_up(ALTIMETERPIN);
 }
+
+void init(){
+    stdio_init_all();
+
+    init_ematch_pin();
+    init_altimeter_pin();
+}
+
+
+/**
+ * Main function
+ */
+int main(){
+
+    int led = 0;
+
+    init();
+    
+    while(true){
+        gpio_put(LED1, 0);
+        
+        // if (led = 0){
+        //     gpio_put(LED1, 1);
+        //     gpio_put(LED3, 0);
+        //     led = 1;
+        // }else if (led = 1){
+        //     gpio_put(LED2, 1);
+        //     gpio_put(LED1, 0);
+        //     led = 2;
+        // }else if (led = 2){
+        //     gpio_put(LED3, 1);
+        //     gpio_put(LED2, 0);
+        //     led = 0;
+        // }
+        
+        if (gpio_get(ALTIMETERPIN)){
+            gpio_put(EMATCHPIN, 1);
+        }
+
+        sleep_ms(1000);
+
+    }
+
+    return 0;
+}
+
+
+/**
+ * Fuction to set up all pins and their directions 
+ * (this would be ~50 lines without the libraries)
+ */
+
+
+
+
+
+
+
